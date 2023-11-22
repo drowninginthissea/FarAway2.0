465de3e67411be8dd1805264d40b37d21bb70e7c
drowning <drowning@DESKTOP-4QRPJV8>
2023-11-22 23:58:09 +0300

    connection string fixed

--- FarAway2.0/Entities/Db.cs
+++ FarAway2.0/Entities/Db.cs
@@ -38,7 +38,7 @@ namespace FarAway2._0.Entities
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseLazyLoadingProxies();
-                optionsBuilder.UseSqlServer("Data Source=1DESKTOP-4QRPJV8\\SQLEXPRESS;" +
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-4QRPJV8\\SQLEXPRESS;" +
                 "Initial Catalog=FarAway;Integrated Security=True;Multiple Active Result Sets=True;" +
                 "TrustServerCertificate=True;");
                 // DESKTOP-2NAFSVT\SQLEXPRESS;
